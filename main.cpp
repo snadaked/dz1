a35c9f46a20af8acad116ef69595345a0f3bf951
snadaked <126073596+snadaked@users.noreply.github.com>
2024-03-10 19:15:55 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,26 +5,22 @@
 #include <string>
 
 int main(int argc, char** argv) {
-    // Проверяем, переданы ли аргументы командной строки
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <path_to_test_file1> <path_to_test_file2> ..." << std::endl;
         return 1;
     }
 
-    // Перебираем все предоставленные пути к файлам
     for (int i = 1; i < argc; ++i) {
         std::string filename = argv[i];
 
         std::cout << "Processing file: " << filename << std::endl;
 
-        // Создание объекта парсера и чтение данных
         TSPParser parser(filename);
         parser.Parse();
 
-        // Получение координат вершин графа
+
         auto coordinates = parser.getCoordinates();
 
-        // Создание объекта солвера и решение задачи коммивояжера
         TSPSolver solver(coordinates);
         solver.solve();
     }
